c4dce177485146d347754f7ec28915fdeaeb3469
DianaG <diana.cg93@gmail.com>
2020-10-21 23:48:25 -0400

    project done

--- script.js
+++ script.js
@@ -20,16 +20,60 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  function shortenTitle () {
+    let title = document.getElementById("main-title");
+    title.textContent = "Dom Toretto's Page"
+  }
+  shortenTitle()
   // Part 2
-
+  function changeBGColor () {
+    document.body.style.backgroundColor = "#4e0fe";
+  }
+  changeBGColor()
   // Part 3
-
+  function removeLastItem () {
+    let favoriteThings = document.getElementById("favorite-things");
+    favoriteThings.removeChild(favoriteThings.lastElementChild)
+  }
+  removeLastItem()
   // Part 4
-
+  function changeFontSize () {
+    let titles = document.querySelectorAll('.special-title');
+    for(let title of titles){
+      title.style.fontSize="2rem"
+    }
+  }
+  changeFontSize()
   // Part 5
-
+  function removeRace () {
+    let races = document.getElementById("past-races").children;
+    for(let race of races){
+      if(race.textContent === "Chicago"){
+        race.remove();
+      }
+    }
+  }
+  removeRace()
   // Part 6
-
+function addRace () {
+  const race = document.createElement('li');
+  race.textContent = "New York";
+  document.getElementById('past-races').appendChild(race);
+}
+addRace();
   // Part 7
+  function createBlogPost () {
+    const text = document.createElement("p");
+    const title = document.createElement("h1");
+    const block = document.createElement("div");
+    text.textContent = "I raced and ate the apple!"
+    title.textContent = "New York"
+    block.className = "blog-post purple"
+    block.appendChild(title)
+    block.appendChild(text)
+    
+    let main = document.querySelector('.main')
+    main.appendChild(block)
+  }
+  createBlogPost()
 }
