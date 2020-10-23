e63d77381ef7e21f3e95cd87eb89b9b11cb31327
Shnaeem <62766356+Shnaeem@users.noreply.github.com>
2020-10-22 22:15:26 -0400

    project5 done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let quotes = [
   `I live my life a quarter mile at a time`,
   `I said a ten-second car, not a ten-minute car`,
@@ -20,16 +21,64 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
+  function shortenTitle () {
+    let title = document.getElementById("main-title");
+    title.textContent = "Dom - Fast & Furious"
+  }
+  shortenTitle()
 
   // Part 2
-
+function color() {
+    document.querySelector(`body`).style.background = "#00cabb";
+  }
+  color();
   // Part 3
-
+  function lastElementt() {
+    let last = document.querySelector(`#favorite-things`)
+    last.removeChild(last.lastElementChild);
+  }
+  lastElementt();
   // Part 4
-
+  function changeSize() {
+    let title = document.getElementsByClassName(`special-title`);
+    for (let i = 0; i < title.length; i++) {
+      let titles = title[i]
+      titles.style.fontSize = "2rem";
+    }
+  }
+  changeSize();
   // Part 5
-
+  function removeRace () {
+    let races = document.getElementById("past-races").children;
+    for(let race of races){
+      if(race.textContent === "Chicago"){
+        race.remove();
+      }
+    }
+  }
+  removeRace()
   // Part 6
-
+  function expRace () {
+    const race = document.createElement('li');
+    race.textContent = "New York City";
+    document.getElementById('past-races').appendChild(race);
+  }
+  expRace();
+  
   // Part 7
+  function createBlogPost () {
+    const text = document.createElement("p");
+    const title = document.createElement("h1");
+    const block = document.createElement("div");
+    text.textContent = "I am the Boss!"
+    title.textContent = "New York City"
+    block.className = "blog-post purple"
+    block.appendChild(title)
+    block.appendChild(text)
+
+    let main = document.querySelector('.main')
+    main.appendChild(block)
+  }
+  createBlogPost()
 }
+
